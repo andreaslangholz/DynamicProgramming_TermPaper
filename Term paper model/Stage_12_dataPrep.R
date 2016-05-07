b9334136c5732fed45b6f77d153e9b9879078e65
andreaslangholz <andreas_a_langholz@hotmail.com>
2016-05-07 20:59:20 +0200

    z

--- Term paper model/Stage_12_dataPrep.R
+++ Term paper model/Stage_12_dataPrep.R
@@ -7,25 +7,26 @@ zdata = read.csv("C:\\Users\\Andreas\\Documents\\GitHub\\DynamicProgramming_Term
 
 # parameters & Initial values -----------------------------
 
-used.areas = c(1:5) # meaning that in the simulated data, option 6 will be outside the
+# The areas under investigation in the model
+used.areas = c(1:5) 
 
-n.periods <- length(unique(zdata$current.year))
-
+# lists of neoghborhoods and years
 neighboorhood <- unique(zdata$area)
-
-n.neighborhoods <- length(used.areas)
-
-n.obs <- nrow(zdata)
-
-moving.costs <- 0.06  # omkostningerne ved at sælge ( undersøg om det er de samme i DK)
-
 years <- unique(zdata$current.year)
 
+# Counting the different vairables
+n.periods <- length(unique(zdata$current.year))
+n.neighborhoods <- length(used.areas)
+n.obs <- nrow(zdata)
+
+# Cost of moving
+moving.costs <- 0.06  
+
+
+# create dummy for outside choice when estimating CCP's
 zdata$outside <- ifelse(zdata$flyt == 1 & zdata$kom.t.1 != used.areas, 1, 0)
-zdata$inside  <- ifelse(zdata$flyt == 1 & zdata$kom.t.1 == used.areas, 1, 0)
 
 # indicators for years and neighborhoods
-
 for (i in 1:n.obs) {
  zdata$year.ind[i] = which(years == zdata$current.year[i])
 }
@@ -34,10 +35,12 @@ for(i in 1:n.obs) {
   zdata$nhood[i] = which(neighboorhood == zdata$area[i])
 }
 
-# Splitting observations in types based on income and wealth ----------------------
+# ------------------------Splitting observations in types based on income and wealth ----------------------
+# Nr of types in each bin
 n.incometypes = 2
 n.wealthtypes = 2
 
+# creating equal length intervals of types
 income.max = max(zdata$income)
 income.min = min(zdata$income)
 income.bins = seq(income.min, income.max, income.max / n.incometypes)
@@ -46,34 +49,35 @@ income.bins[n.incometypes + 1] <- Inf                                # Det maksi
 wealth.max = max(zdata$wealth)
 wealth.min = min(zdata$wealth)
 wealth.bins = seq(wealth.min, wealth.max, wealth.max / n.wealthtypes)
-wealth.bins[n.wealthtypes +1 ] = Inf                                 # det maksimale loft for formue i det sidste led er uendeligt
+wealth.bins[n.wealthtypes + 1] <- Inf                                 # det maksimale loft for formue i det sidste led er uendeligt
 
 n.types <- n.wealthtypes * n.incometypes
 
-type.matrix <- as.data.frame(matrix(1:n.types,n.wealthtypes,n.incometypes))
+# Categorizing the observations in their respective types
+type.matrix <- as.data.frame(matrix(1:n.types, n.wealthtypes, n.incometypes))
 
 for (i in 1:n.obs) {
   for (j in 1:n.incometypes) {
     for (w in 1:n.wealthtypes) {
       
       if(zdata$income[i] >= income.bins[j] & zdata$income[i] < income.bins[j + 1]
-       & zdata$wealth[i] >= wealth.bins[w] & zdata$wealth[i] < wealth.bins[w + 1]) zdata$type.tau[i] <- type.matrix[w,j]
-        
+       & zdata$wealth[i] >= wealth.bins[w] & zdata$wealth[i] < wealth.bins[w + 1]) 
+      {
+        zdata$type.tau[i] <- type.matrix[w,j]
+      }
   }
  } 
 }
 
 
 # New number of types, as we cant loop over types without content ## OBS Tjek lige om det her step er korrekt
-
 n.types <- length(unique(zdata$type.tau))
 
-# Creating the Conditional Choice Probabilities by splittingon time, neighborhoods and years -------------------
+# -------------Creating the Conditional Choice Probabilities by splitting into time, neighborhoods and years -------------------
 
-# Creating frequency tables for observations of each type/year/hood combination and moving decisions
+# constructing frequency tables for of each type/year combination and moving decisions
 
-# nr in each combination
-
+# nr. of obs. in each combination
 group.obs <- tbl_df(zdata) %>%
   group_by(type.tau, year.ind, flyt) %>% 
   summarise(nr = n())
@@ -91,7 +95,7 @@ group.obs$nr <- as.numeric(group.obs$nr)
 
 group.obs$nr[is.na(group.obs$nr)] <- 0
 
-# nr of each combination whith movement
+# nr. of obs. moving
 
 group.obs.move <- tbl_df(zdata) %>%
   group_by(type.tau, year.ind, kom.t.1, flyt) %>% 
@@ -110,7 +114,7 @@ group.obs.move$nr <- as.numeric(group.obs.move$nr)
 
 group.obs.move$nr[is.na(group.obs.move$nr)] <- 0
 
-# nr of each combination moving outside the chosen areas
+# nr. of obs moving outside the chosen areas
 
 group.obs.move.out <- tbl_df(zdata) %>%
   group_by(type.tau, year.ind, outside) %>% 
@@ -129,12 +133,11 @@ group.obs.move.out$nr <- as.numeric(group.obs.move.out$nr)
 
 group.obs.move.out$nr[is.na(group.obs.move.out$nr)] <- 0
 
-# Calculating the % share of move/stay for each type/year/hood combination (man kunne overveje et ekstra if statement for at komme af NaN)
 
+# Finding the CCP's
 shares.moving <- array(0,dim=c(n.types,n.neighborhoods + 1, n.periods))
 
-# Findind CCP's of moving to neighborhood t conditional on moving
-
+# CCP of moving to neighboorhood j conditional on moving
 for (t in 1:n.periods) {
   for (m in 1:n.types) {
      for (j in 1:n.neighborhoods) {
@@ -164,7 +167,8 @@ for (t in 1:n.periods) {
     }
   }
 }
-# outside shares
+
+# CCP of moving outside conditional on moving
 
 for (t in 1:n.periods) {
   for (m in 1:n.types) {
@@ -194,24 +198,15 @@ for (t in 1:n.periods) {
   }
 }
 
-
-
-
-
-shares.moving[,,]
-
 # Creating the ingoing values for the likelihood estimator (the X vector) ---------------------------
 
 # X1 = 1
-
 zdata$x1 = 1
 
 # X2 = income (Psychological Moving Costs)
-
 zdata$x2 = zdata$income
 
 # x3 = 1
-
 zdata$x3 = 1
 
 # x4 = Price_t * moving costs (Financial moving costs)
@@ -219,7 +214,6 @@ zdata$x3 = 1
 # First we need to find the potential price in all periods OBS! Her skal laves en variable for husprisindeks afhængig af område
 
 ## Midlertidig prisudvikling
-
 stignings.proc = 0.017 #midlertidig fra SIM rapport
 
 stig.vec = array()
@@ -239,48 +233,15 @@ for (i in 1:n.obs) {
   }
 }
 
+
 zdata$x4 = zdata$price * moving.costs
 
 # x5 = price * movingcosts * income (Type specific financial moving costs)
-
 zdata$x5 <- zdata$price * moving.costs * zdata$income
 
 # vector of x values for logit
-
 x_sx <- zdata[ ,c("x1","x2","x3","x4","x5")]
 
 y_sx <- zdata[ ,"flyt"]
 
-# Calculating the valuefunction values for each type/year combination -----------------
 
-setwd("C:\\Users\\Andreas\\Documents\\GitHub\\DynamicProgramming_TermPaper\\Term paper model")
-
-source("functions.R")
-
-
-initial.parameters <- c(-9.5, 0.02, 0.1, -0.2, 0.006)
-
-#  Get the estimated value functions by the FOC of MLE problem
-
-value.tilde <- array(rep(NaN, n.types * (n.neighborhoods + 1) * t), c(n.types,(n.neighborhoods + 1), n.periods))
-
-for (m in 1:n.types){
-  print(m)
-  for (t in 1:n.periods){
-    
-    value.tilde[m, , t] <- log(shares.moving[m, , t]) - mean(log(shares.moving[m, ,t]))
-    
-  }
-}
-
-log(0)
-
-log(shares.moving[m, , t]) - mean(log(shares.moving[m, ,t]))
-
-mean(log(shares.moving[2,,1]))
-
-log.sharea = log(shares.moving)
-
-log.sharea[,,]
-
-ml.share = mean(log.sharea[1,,1])
